34752651658f7cd99c1dc55d2aa3b96ca9f3bc1f
Bálint Csertán <csertanb@gmail.com>
2017-12-21 16:42:45 +0100

    Chapter 5 begin

--- gruntfile.js
+++ gruntfile.js
@@ -6,12 +6,68 @@ module.exports = function(grunt) {
   // Project and task configuration
 	grunt.initConfig({  
 		pkg: grunt.file.readJSON('package.json'),
+
+		/*
+		* Grunt Sass
+		* Compile Sass to CSS using node-sass
+		* https://npmjs.com/package/grunt-sass
+		*/
+		sass: {
+	        options: {
+	            sourceMap: true
+	        },
+	        dist: {
+	            files: {
+	                'css/styles.css' : 'assets/scss/styles.scss'
+	            }
+	        }
+	    },
+
+	    uglify: {
+		    my_target: {
+		      files: {
+		        'js/scripts.min.js': ['assets/js/scripts.js', 'node_modules/jquery/dist/jquery.js']
+		      }
+		    }
+		},
+
+	    watch: {
+
+		  sass: {
+
+		    files: [
+		    	'assets/scss/*.scss'
+		    ],
+		    tasks: [
+		    	'sass'
+		    ],
+		    options: {
+		      spawn: false,
+		    },
+		  },
+
+		  scripts: {
+
+		    files: [
+		    	'assets/js/*.js'
+		    ],
+		    tasks: [
+		    	'uglify'
+		    ],
+		    options: {
+		      spawn: false,
+		    },
+		  },
+
+		},
+
+		
 	});
 
   // Loading Grunt plugins and tasks
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+	require('load-grunt-tasks')(grunt);
 
   // Custom tasks
+  grunt.registerTask('default', ['watch']);
 
 };
-
